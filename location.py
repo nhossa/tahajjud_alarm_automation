e60b5eb6b0f9be7ceb65a92343755f78408389b3
nhossa <hossainnaim03@gmail.com>
2025-10-29 19:08:29 -0400

    third commit

--- location.py
+++ location.py
@@ -4,6 +4,8 @@ location = requests.get("http://ip-api.com/json")
 
 location = location.json()
 
+print(location)
+
 # for key, value in location.items():
 #     print(f"{key}: {value}")
 
--- timings.py
+++ timings.py
@@ -3,26 +3,20 @@ from datetime import date,datetime, timedelta
 from location import location
 
 
-BASE_URL= "https://api.aladhan.com/v1/timings/"
-
-# response = requests.get("https://api.aladhan.com/v1/timings/25-10-2025?latitude=39.0503&longitude=-77.3909&timezonestring=America/New_York&method=2")
-
-
 
 today = date.today()
 today = today.strftime("%d-%m-%Y")
+
+BASE_URL= f"https://api.aladhan.com/v1/timings/{today}"
 
 latitude = location["lat"]
 longitude = location["lon"]
 timzone = location["timezone"]
 
-url = f"{BASE_URL}&date={today}?&latitude={latitude}&longitude={longitude}&timezonestring={timzone}&method=2"
-print(url)
+# 2 - Islamic Society of North America calculation method
+url = f"{BASE_URL}?latitude={latitude}&longitude={longitude}&timezonestring={timzone}&method=2"
 response = requests.get(url)
-print(response.status_code)
 timings = response.json()
-fajr_time = timings['data']['timings']['Fajr']
-fajr_time = datetime.strptime(fajr_time, "%H:%M")
-tahajjud_time = (fajr_time - timedelta(minutes=30)).time()
 
-print(tahajjud_time)
+fajr_time = timings['data']['timings']['Fajr']
+print(fajr_time)
